cf6ce0ca81d0042cc97c1fd8dc36ffcda05cf566
janinay0714 <118411601+janinay0714@users.noreply.github.com>
2022-11-21 11:39:15 +0800

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,226 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Refers to the study that integrates electronic engineering with computer sciences to design and develop computer systems and other technological devices. ?",
+    answer: "Computer Engineering",
+    options: [
+      "Computer Science",
+      "Information Technology",
+      "Computer Engineering",
+      "Civil Engineering"
+    ]
+  },
+    {
+    numb: 2,
+    question: "It is a way for programmers and/or developers to communicate with computer?",
+    answer: "Programming Language",
+    options: [
+      "English Language",
+      "Tagalog Language",
+      "Sign Language",
+      "Programming Language"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "It is an integrated circuit that contains a microprocessor along with memory and associated circuits and that controls some or all of the functions of an electronic device (such as a home appliance) or system?",
+    answer: "Microcontroller",
+    options: [
+      "Microcontroller",
+      "Website",
+      "Computer",
+      "Personal Computer"
+    ]
+  },
+    {
+    numb: 5,
+    question: "It refers to the external and internal devices and equipment that enable you to perform major functions such as input, output, storage, communication, processing, and more.",
+    answer: "Hardware",
+    options: [
+      "Software",
+      "Hardware",
+      "Courseware",
+      "Silverware"
+    ]
+  },
+   {
+  numb: 6,
+ question: "It is a non-contact type device that can be used to measure distance as well as velocity of an object and works based on the properties of the sound waves with frequency greater than that of the human audible range.",
+  answer: "Ultrasonic Sensor",
+  options: [
+   "Light Sensor",
+   "Sound Sensor",
+   "Ultrasonic Sensor",
+   "Proximity Sensor"
+     ]
+   },
+   {
+   numb: 7,
+      question: "What does SQL stand for?",
+      answer: "Structured Query Language",
+      options: [
+        "Stylish Question Language",
+        "Stylesheet Query Language",
+        "Statement Question Language",
+        "Structured Query Language"
+      ]
+    }, 
+    {
+    numb: 8,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+      ]
+    },    
+      {
+    numb: 9,
+    question: "What sensor is used in breathalyzer devices, which determine whether a person is drunk or not?",
+    answer: "Alcohol Sensor",
+    options: [
+     "Fire Sensor",
+     "Humidity Sensor",
+     "Alcohol Sensor",
+     "Ultrasonic Sensor"
+    ]
+    },
+      {
+    numb: 10,
+    question: "Which of the following is an example of a programming language?",
+    answer: "C#",
+    options: [
+    "Dell",
+    "Computer",
+    "Linux",
+    "C#"
+    ]
+    },            
+    {
+    numb: 11,
+    question: "These are devices that can measure a physical quantity and convert it into a signal, which can be read and interpreted by the microcontroller unit?",
+    answer: "Sensors",
+    options: [
+    "Computer",
+    "Sensors",
+    "Printer",
+    "Scanner"
+    ]
+    },            
+    
+  {
+    numb: 12,
+    question: "Devices can connect directly to the Internet?",
+    answer: "WiFi",
+    options: [
+    "WiFi",
+    "Mousepad",
+    "Coupon",
+    "Portable Hardrive"
+   ]
+   },
+
+   {
+    numb: 13,
+    question: "Which of the following is an example of Operating System?",
+    answer: "Linux",
+    options: [
+    "Computer",
+    "Keyboard",
+    "Printer",
+    "Linux"
+   ]
+},
+   {
+    numb: 14,
+    question: "It is a non-conductive material with conductive lines printed or etched?",
+    answer: "Printed-Circuit-Board",
+    options: [
+    "CPU",
+    "Printed-Circuit-Board",
+    "Hardrive",
+    "Scanner"
+   ]
+},
+   {
+    numb: 15,
+    question: "It sends information to a computer system for processing?",
+    answer: "Input",
+    options: [
+    "Input",
+    "Output",
+    "Storage",
+    "Flashdrive"
+   ]
+ },
+   {
+    numb: 16,
+    question: "This devices reproduce or display the results of that processing?",
+    answer: "Output",
+    options: [
+    "Input",
+    "Output",
+    "Storage",
+    "Flashdrive"
+   ]
+},
+   {
+    numb: 17,
+    question: "The following are examples of input devices, except?",
+    answer: "Projectors",
+    options: [
+    "Keyboard",
+    "Mouse",
+    "Joystick",
+    "Projectors"
+   ]
+},
+   {
+    numb: 18,
+    question: "The following are examples of output devices, except?",
+    answer: "Scanner",
+    options: [
+    "Printer",
+    "Monitor",
+    "Headphone",
+    "Scanner"
+   ]
+   },
+{
+    numb: 19,
+    question: "It contains a text editor for writing code, a message area, a text console, a toolbar with buttons for common functions and a series of menus?",
+    answer: "Arduino IDE",
+    options: [
+    "Software",
+    "Hardware",
+    "Arduino IDE",
+    "Computer"
+   ]
+},
+  {
+   numb: 20,
+   question: "The name of the first computer virus created in the Philippines is called?",
+   answer: "I love you virus",
+   options: [
+   "Morris Worm",
+   "I love you virus",
+   "CryptoLocker",
+   "Malware"
+  ]
+  },                
+];
+
